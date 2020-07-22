5dbdf8ac0853f07a965c771c7a7ce22c13649563
hwalker928 <harry@repoleved.xyz>
2020-07-22 10:34:31 +0100

    Update memes.py

--- cogs/memes.py
+++ cogs/memes.py
@@ -93,29 +93,5 @@ class Memes(commands.Cog):
         except Exception as e:
             await ctx.send(f"An error occurred while attempting to perform that command.\nInfo: `{e}`")
 
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.content.startswith("r/"):
-            msg = message.content.split("/")
-            subreddit = msg[1]
-            try:
-                while True:
-                    request = "https://meme-api.herokuapp.com/gimme/" + subreddit
-                    r = requests.get(request)
-                    a = r.json()
-                    if a["nsfw"] == False:
-                        title = a["title"]
-                        embed = discord.Embed(title=title)
-                        embed.set_image(url=a["url"])
-                        current_time = datetime.datetime.now()
-                        embed.set_footer(text="Requested by " + message.author.name + " • " + str(current_time.day) + "/" + str(
-                            current_time.month) + "/" + str(current_time.year), icon_url=message.author.avatar_url)
-                        await message.channel.send(embed=embed)
-                        break
-                else:
-                    pass
-            except Exception as e:
-                await message.channel.send(f"An error occurred while attempting to perform that command.\nInfo: `{e}`")
 def setup(bot):
     bot.add_cog(Memes(bot))
