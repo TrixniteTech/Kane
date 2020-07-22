d578b13bf915b6ade8eba8341edcf4a63c2c7da9
hwalker928 <harry@repoleved.xyz>
2020-07-22 08:58:21 +0100

    Replaced invitegen with Kane

--- cogs/invitegen.py
+++ cogs/invitegen.py
@@ -7,7 +7,7 @@ class InviteGen(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def invitegen(self, ctx, id=735399466688970763):
+    async def invitegen(self, ctx, id=734839312675242116):
         await ctx.send(f"https://discord.com/api/oauth2/authorize?client_id={id}&permissions=8&scope=bot")
 
 def setup(bot):
