37032d582df4d09129b309c509583822bdefff10
hwalker928 <harry@repoleved.xyz>
2020-07-30 22:55:12 +0100

    Update system.py

--- cogs/system.py
+++ cogs/system.py
@@ -3,34 +3,61 @@ from discord.ext import commands
 import datetime
 import psutil
 from pathlib import Path
+from psutil import virtual_memory
 
 class System(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(aliases=['si', 'server', 'harryserver', 'sinfo', 'system'])
     async def systeminfo(self, ctx):
         if ctx.author.id == 428450288668508160:
-                cpu_per = round(psutil.cpu_percent(),1) # Get CPU Usage
-                mem = psutil.virtual_memory()
-                mem_per = round(psutil.virtual_memory().percent,1)
-                embed=discord.Embed(title="Server stats")
-                embed.add_field(name="Memory usage:", value=str(mem_per)+"%", inline=False)
-                embed.add_field(name="CPU usage:", value=str(cpu_per)+"%", inline=True)
-                await ctx.send(embed=embed)
+                ram_usage = round(psutil.virtual_memory().percent,1)
+                cpu_usage = round(psutil.cpu_percent(),1)
+                disk = psutil.disk_usage('/')
+                if cpu_usage < 49:
+                    embed=discord.Embed(color=0x35e35c)
+                    embed.add_field(name="Memory usage:", value=str(ram_usage)+"%", inline=True)
+                    embed.add_field(name="CPU usage:", value=str(cpu_usage)+"%", inline=True)
+                    embed.add_field(name="Core used:", value=str(psutil.Process().cpu_num()), inline=False)
+                    embed.add_field(name="Process:", value=str(psutil.Process().exe()), inline=False)
+                    embed.add_field(name="Disk used:", value=str(disk.percent) + "%", inline=False)
+                    embed.add_field(name="Boot time:", value=str(datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")), inline=False)
+                    await ctx.send(embed=embed)
+                else:
+                    embed=discord.Embed(color=0xeb452e)
+                    embed.add_field(name="Memory usage:", value=str(ram_usage)+"%", inline=True)
+                    embed.add_field(name="CPU usage:", value=str(cpu_usage)+"%", inline=True)
+                    embed.add_field(name="Core used:", value=str(psutil.Process().cpu_num()), inline=False)
+                    embed.add_field(name="Process:", value=str(psutil.Process().exe()), inline=False)
+                    embed.add_field(name="Boot time:", value=str(datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")), inline=False)
+                    await ctx.send(embed=embed)        
 
     @commands.command()
-    async def reload(self, ctx):
+    async def reload(self, ctx, item="all"):
         if ctx.author.id == 428450288668508160:
-            cogs = [x.stem for x in Path('cogs').glob('*.py')]
-            for extension in cogs:
+            if item == "all":
+                cogs = [x.stem for x in Path('cogs').glob('*.py')]
+                for extension in cogs:
+                    try:
+                        self.bot.reload_extension(f'cogs.{extension}')
+                        print(f'reloaded {extension}')
+                        embed=discord.Embed(title=f"reloaded {extension}", color=0x35e35c)
+                        await ctx.send(embed=embed)
+                    except Exception as e:
+                        error = f'{extension}\n {type(e).__name__} : {e}'
+                        print(f'failed to reload extension {error}')
+                        embed=discord.Embed(title=f"failed to reload extension {error}", color=0xeb452e)
+                        await ctx.send(embed=embed)
+                    print('-' * 10)
+            else:
                 try:
-                    self.bot.reload_extension(f'cogs.{extension}')
-                    print(f'reloaded {extension}')
-                    embed=discord.Embed(title=f"reloaded {extension}", color=0x35e35c)
+                    self.bot.reload_extension(f'cogs.{item}')
+                    print(f'reloaded {item}')
+                    embed=discord.Embed(title=f"reloaded {item}", color=0x35e35c)
                     await ctx.send(embed=embed)
                 except Exception as e:
-                    error = f'{extension}\n {type(e).__name__} : {e}'
+                    error = f'{item}\n {type(e).__name__} : {e}'
                     print(f'failed to reload extension {error}')
                     embed=discord.Embed(title=f"failed to reload extension {error}", color=0xeb452e)
                     await ctx.send(embed=embed)
